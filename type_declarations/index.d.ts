02c0f19b10a49c94679147105d6352e41b93e0b7
Christopher Brown <io@henrian.com>
2015-07-18 17:05:33 -0500

    Rearrange type declarations

---
+++ type_declarations/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="DefinitelyTyped/async/async.d.ts" />
+/// <reference path="DefinitelyTyped/node/node.d.ts" />
+/// <reference path="DefinitelyTyped/request/request.d.ts" />
+
+/// <reference path="../node_modules/regex-router/regex-router.d.ts" />
