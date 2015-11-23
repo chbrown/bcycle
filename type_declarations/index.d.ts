9344f1ce6dfdfa506e6ae0f8ca981cfb9afe58b8
Christopher Brown <io@henrian.com>
2015-11-22 22:57:13 -0600

    Remove local type_declarations/

--- type_declarations/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="DefinitelyTyped/async/async.d.ts" />
-/// <reference path="DefinitelyTyped/node/node.d.ts" />
-/// <reference path="DefinitelyTyped/request/request.d.ts" />
-/// <reference path="DefinitelyTyped/moment/moment.d.ts" />
-
-/// <reference path="../node_modules/regex-router/regex-router.d.ts" />
